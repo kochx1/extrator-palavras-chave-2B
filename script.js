55186511c2cab35fd6fd7a85bbd0c4f878814e82
kochx1 <koch.arthur@escola.pr.gov.br>
2025-11-04 09:27:21 -0300

    04-11

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 const botaoMostraPalavras = document.querySelector("#botao-palavrachave");
 
 botaoMostraPalavras.addEventListener("click", mostraPalavrasChave);
@@ -6,7 +5,35 @@ botaoMostraPalavras.addEventListener("click", mostraPalavrasChave);
 function mostraPalavrasChave() {
     const texto = document.querySelector("#entrada-de-texto").value;
     const campoResultado = document.querySelector("#resultado-palavrachave");
-    const palavras = texto.split(" ");
+    const palavrasChave = processaTexto(texto);
 
-    campoResultado.textContent = palavras.join(", ");
+    campoResultado.textContent = palavrasChave.join(", ");
+}
+
+function processaTexto(texto) {
+    let palavras = texto.split(/\P{L}+/u);
+    const frequencias = contaFrequencias(palavras);
+    let ordenadas = Object.keys(frequencias).sort(ordenaPalavra);
+
+    function ordenaPalavra(p1, p2) {
+        return frequencias[p2] - frequencias[p1];
+    }
+
+    return ordenadas.slice(0, 10);
+}
+
+function contaFrequencias(palavras) {
+    let frequencias = {};
+
+    for (let palavra of palavras) {
+        if (palavra.trim() === "") continue;
+        palavra = palavra.toLowerCase();
+        if (frequencias[palavra]) {
+            frequencias[palavra]++;
+        } else {
+            frequencias[palavra] = 1;
+        }
+    }
+
+    return frequencias;
 }
